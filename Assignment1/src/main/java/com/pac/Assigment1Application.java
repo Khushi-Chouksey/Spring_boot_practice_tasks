499825558e3ba7a96cf3a507e4a41744eee30c90
Khushi Chouksey <choukseykhushi387@gmail.com>
2025-04-08 20:09:23 +0530

    Initial commit with assigmnet1

---
+++ Assignment1/src/main/java/com/pac/Assigment1Application.java
@@ -0,0 +1,17 @@
+package com.pac;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class Assigment1Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assigment1Application.class, args);
+    }
+
+
+}
